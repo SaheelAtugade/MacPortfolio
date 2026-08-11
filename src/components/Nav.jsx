dba5b6d89092033f951b33522927a7d25bd3e5c9
dev-saheel <sahilatugade212@gmail.com>
2026-08-11 18:56:38 +0530

    final commits

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,41 +1,54 @@
-import React from 'react'
-import './nav.scss'
-import DateTime from './DateTime'
+import React from "react";
+import "./nav.scss";
+import DateTime from "./DateTime";
+import { useWindowStateContext } from "./context/WindowStateContext";
 
-const Nav = ({ windowState }) => {
-
-   const isAnyWindowOpen = Object.values(windowState).some(value => value === "maximize");
+const Nav = () => {
+  const { windowState, setWindowState } = useWindowStateContext();
+  const isAnyWindowOpen = Object.values(windowState).some(
+    (value) => value === "maximize",
+  );
 
   return (
-    <nav style={isAnyWindowOpen?{zIndex: 0}:{zIndex: 20}}>
-        <div className="left">
-            <div className="nav-icon">
-                <img src="/navbar_icon/apple.svg" alt="" />
-            </div>
-            
-            <div className="nav-item">
-                <p>Saheel Atugade</p>
-            </div>
-            <div className="nav-item">
-                <p>file</p>
-            </div>
-            <div className="nav-item">
-                <p>window</p>
-            </div>
-            <div className="nav-item">
-                <p>terminal</p>
-            </div>
+    <nav style={isAnyWindowOpen ? { zIndex: 0 } : { zIndex: 20 }}>
+      <div className="left">
+        <div className="nav-icon">
+          <img src="/navbar_icon/apple.svg" alt="" />
         </div>
-        <div className="right">
-            <div className="nav-icon">
-                <img src="/navbar_icon/wifi.svg" alt="" />
-            </div>
-            <div className="nav-item">
-                <DateTime/>
-            </div>
-        </div>
-    </nav>
-  )
-}
 
-export default Nav
+        <div className="nav-item">
+          <p>Saheel Atugade</p>
+        </div>
+        <div className="nav-item">
+          <p
+            onClick={() => window.open("https://github.com/SaheelAtugade", "_blank")}
+          >Github</p>
+        </div>
+        <div className="nav-item">
+          <p
+            onClick={() => window.open("https://www.linkedin.com/in/sahil-atugade/", "_blank")}
+          >Linkedin</p>
+        </div>
+        <div className="nav-item">
+          <p
+            onClick={() => {
+              setWindowState((prev) => ({ ...prev, cli: "minimize" }));
+            }}
+          >
+            Terminal
+          </p>
+        </div>
+      </div>
+      <div className="right">
+        <div className="nav-icon">
+          <img src="/navbar_icon/wifi.svg" alt="" />
+        </div>
+        <div className="nav-item">
+          <DateTime />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
